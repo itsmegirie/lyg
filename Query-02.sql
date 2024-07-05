ce7ed8b74c962549452692b37efe0c1ff0687098
root <root@sierra.zaviera.local>
2024-07-05 08:38:41 +0000

    add assets/adminLTE

--- Query-02.sql
+++ Query-02.sql
@@ -1,14 +1,17 @@
+DROP TABLE IF EXISTS lygDestination; 
 CREATE TABLE lygDestination(
     DestinationCode CHAR(2) NOT NULL PRIMARY KEY,
     DestinationName VARCHAR(50) DEFAULT NULL
 );
 
+DROP TABLE IF EXISTS lygStyleSize;
 CREATE TABLE lygStyleSize(
     StyleCode       VARCHAR(50) NOT NULL,
     SizeSort        INT NOT NULL,
     SizeName        VARCHAR(10) NOT NULL
 );
 
+DROP TABLE IF EXISTS lygSewingOutput;
 CREATE TABLE lygSewingOutput(
     TrnDate         DATE NOT NULL,
     OperatorName    VARCHAR(50) NOT NULL,
