1d961b27770df7f502d099db39243b69ecda3a38
root <root@sierra.zaviera.local>
2024-07-05 12:39:20 +0000

    add controller sewing.php & view sewing.php

---
+++ application/views/sewing.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>LYG WEB | Sewing</title>
+
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="<?= base_url('assets/AdminLTE/plugins/fontawesome-free/css/all.min.css'); ?>">
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets/AdminLTE/dist/css/adminlte.min.css'); ?>">
+</head>
+
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+                </li>
+                <li class="nav-item d-none d-sm-inline-block">
+                    <a href="index3.html" class="nav-link"><span class="fas fa-home"></span></a>
+                </li>
+                <li class="nav-item active d-none d-sm-inline-block">
+                    <a href="#" class="nav-link">OUTPUT</a>
+                </li>
+            </ul>
+        </nav>
+
+
+
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <div class="sidebar">
+                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                    <div class="image">
+                        <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                    </div>
+                    <div class="info">
+                        <a href="#" class="d-block">Giri T Dwiputra</a>
+                    </div>
+                </div>
+
+                <div class="form-inline">
+                    <div class="input-group" data-widget="sidebar-search">
+                        <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+                        <div class="input-group-append">
+                            <button class="btn btn-sidebar">
+                                <i class="fas fa-search fa-fw"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                        <li class="nav-item active">
+                            <a href="pages/gallery.html" class="nav-link">
+                                <i class="nav-icon fas fa-book"></i>
+                                <p>
+                                    Sewing
+                                </p>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+
+            </div>
+
+        </aside>
+
+
+        <div class="content-wrapper">
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Summary</h3>
+                        </div>
+                        <div class="card-body">
+                            <table id="table_anggota" class="table table-sm table-bordered table-striped table-hover">
+                                <thead>
+                                    <tr>
+                                        <th class="text-center">Date</th>
+                                        <th class="text-center">Style</th>
+                                        <th class="text-center">Total Size</th>
+                                        <th class="text-center">Total Output</th>
+                                        <th class="text-center">Action</th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- <div class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="card">
+                                <div class="card-header border-0">
+                                    <div class="d-flex justify-content-between">
+                                        <h3 class="card-title">Online Store Visitors</h3>
+                                        <a href="javascript:void(0);">View Report</a>
+                                    </div>
+                                </div>
+                                <div class="card-body">
+                                    <div class="d-flex">
+                                        <p class="d-flex flex-column">
+                                            <span class="text-bold text-lg">820</span>
+                                            <span>Visitors Over Time</span>
+                                        </p>
+                                        <p class="ml-auto d-flex flex-column text-right">
+                                            <span class="text-success">
+                                                <i class="fas fa-arrow-up"></i> 12.5%
+                                            </span>
+                                            <span class="text-muted">Since last week</span>
+                                        </p>
+                                    </div>
+
+
+                                    <div class="position-relative mb-4">
+                                        <canvas id="visitors-chart" height="200"></canvas>
+                                    </div>
+
+                                    <div class="d-flex flex-row justify-content-end">
+                                        <span class="mr-2">
+                                            <i class="fas fa-square text-primary"></i> This Week
+                                        </span>
+
+                                        <span>
+                                            <i class="fas fa-square text-gray"></i> Last Week
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div class="card">
+                                <div class="card-header border-0">
+                                    <h3 class="card-title">Products</h3>
+                                    <div class="card-tools">
+                                        <a href="#" class="btn btn-tool btn-sm">
+                                            <i class="fas fa-download"></i>
+                                        </a>
+                                        <a href="#" class="btn btn-tool btn-sm">
+                                            <i class="fas fa-bars"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="card-body table-responsive p-0">
+                                    <table class="table table-striped table-valign-middle">
+                                        <thead>
+                                            <tr>
+                                                <th>Product</th>
+                                                <th>Price</th>
+                                                <th>Sales</th>
+                                                <th>More</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>
+                                                    <img src="dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
+                                                    Some Product
+                                                </td>
+                                                <td>$13 USD</td>
+                                                <td>
+                                                    <small class="text-success mr-1">
+                                                        <i class="fas fa-arrow-up"></i>
+                                                        12%
+                                                    </small>
+                                                    12,000 Sold
+                                                </td>
+                                                <td>
+                                                    <a href="#" class="text-muted">
+                                                        <i class="fas fa-search"></i>
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>
+                                                    <img src="dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
+                                                    Another Product
+                                                </td>
+                                                <td>$29 USD</td>
+                                                <td>
+                                                    <small class="text-warning mr-1">
+                                                        <i class="fas fa-arrow-down"></i>
+                                                        0.5%
+                                                    </small>
+                                                    123,234 Sold
+                                                </td>
+                                                <td>
+                                                    <a href="#" class="text-muted">
+                                                        <i class="fas fa-search"></i>
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>
+                                                    <img src="dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
+                                                    Amazing Product
+                                                </td>
+                                                <td>$1,230 USD</td>
+                                                <td>
+                                                    <small class="text-danger mr-1">
+                                                        <i class="fas fa-arrow-down"></i>
+                                                        3%
+                                                    </small>
+                                                    198 Sold
+                                                </td>
+                                                <td>
+                                                    <a href="#" class="text-muted">
+                                                        <i class="fas fa-search"></i>
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>
+                                                    <img src="dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
+                                                    Perfect Item
+                                                    <span class="badge bg-danger">NEW</span>
+                                                </td>
+                                                <td>$199 USD</td>
+                                                <td>
+                                                    <small class="text-success mr-1">
+                                                        <i class="fas fa-arrow-up"></i>
+                                                        63%
+                                                    </small>
+                                                    87 Sold
+                                                </td>
+                                                <td>
+                                                    <a href="#" class="text-muted">
+                                                        <i class="fas fa-search"></i>
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div> -->
+
+        </div>
+        <footer class="main-footer">
+            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+            All rights reserved.
+            <div class="float-right d-none d-sm-inline-block">
+                <b>Version</b> 3.2.0
+            </div>
+        </footer>
+    </div>
+
+    <script src="<?= base_url('assets/AdminLTE/plugins/jquery/jquery.min.js'); ?>"></script>
+    <script src="<?= base_url('assets/AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
+    <script src="<?= base_url('assets/AdminLTE/dist/js/adminlte.js'); ?>"></script>
+</body>
+
+</html>
